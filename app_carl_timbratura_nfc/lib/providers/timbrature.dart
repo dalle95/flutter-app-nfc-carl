682e063742b157d3094bd37fbc68a7a94a64778a
Simone Dalle Vedove <simonedallevedove@hotmail.it>
2023-05-23 14:04:23 +0200

    Timbratura Tag NFC - Updated

--- app_carl_timbratura_nfc/lib/providers/timbrature.dart
+++ app_carl_timbratura_nfc/lib/providers/timbrature.dart
@@ -54,7 +54,7 @@ class Timbrature with ChangeNotifier {
 
     // Definizione del link della chiamata
     var url = Uri.parse(
-      '$urlAmbiente/api/entities/v1/road?include=box&filter[utente.code]=${user!.code}',
+      '$urlAmbiente/api/entities/v1/road?include=box&filter[utente.code]=${user!.code}&page[limit]=100',
     );
 
     // Preparazione del try-catch degli errori

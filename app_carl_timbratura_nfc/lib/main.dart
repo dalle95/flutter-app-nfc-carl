dadd8aba567c7fbddc9a8a4157eb90fde8f2bb7e
Simone Dalle Vedove <simonedallevedove@hotmail.it>
2023-05-23 13:09:42 +0200

    Timbratura Tag NFC - Updated

--- app_carl_timbratura_nfc/lib/main.dart
+++ app_carl_timbratura_nfc/lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import '../themes/app_theme.dart';
